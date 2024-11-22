78644edb82b57c1e25a533da38bf1f9a1781b8c4
Krista Goralczyk <krista.goralczyk@gmail.com>
2024-11-22 15:57:19 -0500

    refactor Pigeonburgh to react v18

--- src/Pigeonburgh.js
+++ src/Pigeonburgh.js
@@ -1,104 +1,90 @@
 // React & third party libaries
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Modal from 'react-modal';
 
 // Data
 import storyline from './data/storyline.json';
-import inventory from './data/inventory.json';
+// import inventory from './data/inventory.json';
 
 // Components
 import Step from './components/Step';
 import Substep from './components/Substep';
-import Inventory from './components/Inventory';
+// import Inventory from './components/Inventory';
 
 // Styling
 import { Pigeonwrap, modalStyles } from './styles/styles';
 
 Modal.setAppElement('#root');
 
-class Pigeonburgh2 extends Component {
-	state = {
-		step: 1,
-		currentParagraph: 0,
-		currentSubstep: 0, // substep 0 is technically "no substep". Ground 0.
-		substepModalOpen: false,
-		viewingInventory: false,
-		steps: storyline.steps,
-		allInvetory: inventory.items,
-		inventory: [], // user's current inventory - gets populated as story progresses via info from step choice
-	};
+export default function Pigeonburgh() {
+	const [step, setStep] = useState(1);
+	const [paragraph, setParagraph] = useState(0);
+	const [substep, setSubstep] = useState(0);
+	const [modalOpen, toggleModal] = useState(false);
+	// const [inventoryOpen, toggleInventory] = useState(false);
+	// const [inventory, setInventory] = useState([]);
+	
+	const stepData = storyline.steps;
+	// const inventoryData = inventory.items;
 
-	changeStep = (step) => this.setState({ step, currentSubstep: 0, currentParagraph: 0 });
-
-	changeSubstep = (currentSubstep) => {
-		this.setState({ currentSubstep, substepModalOpen: true });
-	};
-
-	updateCurrentParagraph = () => {
-		this.setState((prevState) => {
-			return { currentParagraph: prevState.currentParagraph + 1 };
-		});
-	};
-
-	closeSubstepModal = () => {
-		this.changeSubstep(0);
-		this.setState({ substepModalOpen: false });
-	};
-
-	toggleInventory = () => this.setState({ viewingInventory: !this.state.viewingInventory });
-
-	renderInventoryButton = () => {
-		const { inventory, viewingInventory } = this.state;
-		if (inventory.length && !viewingInventory) {
-			return (
-				<button onClick={() => this.toggleInventory()} style={{ marginTop: 25 }}>
-					View Inventory
-				</button>
-			);
-		}
-	};
-
-	renderContent = () => {
-		const { step, currentParagraph, currentSubstep, steps, viewingInventory } = this.state;
-		const { content, substeps, choices } = steps.find((data) => data.id === step);
-		if (viewingInventory) {
-			return <Inventory inventory={this.state.inventory} toggleInventory={this.toggleInventory} />;
-		}
-		return (
-			<Step
-				content={content}
-				substeps={substeps}
-				choices={choices}
-				changeStep={this.changeStep}
-				changeSubstep={this.changeSubstep}
-				currentParagraph={currentParagraph}
-				currentSubstep={currentSubstep}
-				updateCurrentParagraph={this.updateCurrentParagraph}
-			/>
-		);
-	};
-
-	render() {
-		const { step, steps, currentSubstep, substepModalOpen } = this.state;
-		const { substeps } = steps.find((data) => data.id === step);
-		const foundSubstep = substeps.find((obj) => obj.id === currentSubstep);
-		const substepText = foundSubstep ? foundSubstep.text : '';
-		return (
-			<div>
-				<Pigeonwrap>
-					{this.renderContent()}
-					<Modal
-						isOpen={substepModalOpen}
-						onRequestClose={this.closeSubstepModal}
-						style={modalStyles}
-						contentLabel={substepText}>
-						<Substep text={substepText} closeSubstepModal={this.closeSubstepModal} />
-					</Modal>
-					{this.renderInventoryButton()}
-				</Pigeonwrap>
-			</div>
-		);
+	function changeStep(step) {
+		setStep(step);
+		setSubstep(0);
+		setParagraph(0);
 	}
-}
 
-export default Pigeonburgh2;
+	function changeSubstep(substep) {
+		setSubstep(substep);
+		toggleModal(true);
+	}
+
+	function updateCurrentParagraph() {
+		setParagraph(paragraph + 1);
+	}
+
+	function closeSubstepModal() {
+		setSubstep(0);
+		toggleModal(false);
+	}
+
+	// function handleInventory() {
+	// 	toggleInventory(!inventoryOpen);
+	// }
+
+	const { content, substeps, choices } = stepData.find((data) => data.id === step);
+	const foundSubstep = substeps.find((obj) => obj.id === substep);
+	const substepText = foundSubstep ? foundSubstep.text : '';
+	return (
+		<div>
+			<Pigeonwrap>
+				{/* main story step */}
+				<Step
+					content={content}
+					substeps={substeps}
+					choices={choices}
+					changeStep={changeStep}
+					changeSubstep={changeSubstep}
+					currentParagraph={paragraph}
+					currentSubstep={substep}
+					updateCurrentParagraph={updateCurrentParagraph}
+				/>
+				{/* substep modal */}
+				<Modal
+					isOpen={modalOpen}
+					onRequestClose={closeSubstepModal}
+					style={modalStyles}
+					contentLabel={substepText}>
+					<Substep
+						text={substepText}
+						closeSubstepModal={closeSubstepModal}
+					/>
+				</Modal>
+				{/* {inventory && inventory.length (
+					<button onClick={handleInventory} style={{ marginTop: 25 }}>
+						View Inventory
+					</button>						
+				)} */}
+			</Pigeonwrap>
+		</div>
+	);
+}
